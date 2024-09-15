d32080a2bed0cb4ec7206b7bd5d70571e6470c25
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-09-15 22:02:45 +0300

    feat: check confirmation code on sol transfer

--- programs/solana-safe-transfer/src/lib.rs
+++ programs/solana-safe-transfer/src/lib.rs
@@ -1,4 +1,7 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::sysvar};
+
+mod errors;
+use errors::TransferError::InvalidConfirmationCode;
 
 declare_id!("Enj1cDwz8kDR95U2SayoDG7LXNdvxCKgNguV2gXHy2fV");
 
@@ -8,9 +11,79 @@ pub mod solana_safe_transfer {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+
+        let confirmation_account = &mut ctx.accounts.confirmation_account;
+        confirmation_account.confirmation_code = "Hello".to_string();
+
+        msg!("Confirmation Account initialized");
+        msg!(
+            "Comfirmation Code: {:?}",
+            confirmation_account.confirmation_code
+        );
+
+        Ok(())
+    }
+
+    pub fn transfer_sol(
+        ctx: Context<TransferSol>,
+        amount: u64,
+        confirmaction_code: String,
+    ) -> Result<()> {
+        // Transfer SOL
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.from.key(),
+            &ctx.accounts.to.key(),
+            amount,
+        );
+
+        let confirmation_account = &ctx.accounts.confirmation_account;
+
+        if confirmaction_code != confirmation_account.confirmation_code {
+            return Err(InvalidConfirmationCode.into());
+        }
+
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.from.to_account_info(),
+                ctx.accounts.to.to_account_info(),
+            ],
+        )?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+
+    #[account(init, payer = signer, space= ConfirmationAccount::INIT_SPACE, seeds = [signer.key().as_ref(), b"ca"], bump)]
+    pub confirmation_account: Account<'info, ConfirmationAccount>,
+    /// CHECK: account constraints checked in account trait
+    #[account(address = sysvar::slot_hashes::id())]
+    recent_slothashes: UncheckedAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct TransferSol<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub to: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+
+    #[account(seeds = [to.key().as_ref(), b"ca"], bump)]
+    pub confirmation_account: Account<'info, ConfirmationAccount>,
+}
+
+#[account]
+pub struct ConfirmationAccount {
+    confirmation_code: String,
+}
+impl Space for ConfirmationAccount {
+    // anchor_discriminator + string_prefix + string
+    const INIT_SPACE: usize = 8 + 4 + 10;
+}
--- tests/solana-safe-transfer.ts
+++ tests/solana-safe-transfer.ts
@@ -1,16 +1,204 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaSafeTransfer } from "../target/types/solana_safe_transfer";
+import { Keypair, Transaction, Signer } from "@solana/web3.js";
+import { expect } from "chai";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 describe("solana-safe-transfer", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaSafeTransfer as Program<SolanaSafeTransfer>;
+  const program = anchor.workspace
+    .SolanaSafeTransfer as Program<SolanaSafeTransfer>;
+  const connection = anchor.getProvider().connection;
 
-  it("Is initialized!", async () => {
+  const owner = anchor.getProvider().publicKey.toString();
+  // @ts-expect-error
+  const wallet = anchor.getProvider().wallet as any;
+
+  it.skip("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        signer: wallet.publicKey,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it("Should transfer SOL with the right confirmation code", async () => {
+    const sender = new Keypair();
+    console.log("Sender", sender.publicKey.toString());
+
+    // get pda address
+    const [pda, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [wallet.publicKey.toBuffer(), Buffer.from("ca")],
+      program.programId
+    );
+
+    console.log("PDA", pda.toString());
+
+    // initialize sender account and transfer SOL to it
+    const tx = new Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: sender.publicKey,
+        lamports: 1000000000,
+        space: 0,
+        programId: anchor.web3.SystemProgram.programId,
+      })
+    );
+
+    const sig0 = await connection.sendTransaction(tx, [wallet.payer, sender]);
+    await connection.confirmTransaction(sig0, "confirmed");
+    console.log("Create account", sig0);
+
+    const senderBalance = await connection.getBalance(
+      sender.publicKey,
+      "finalized"
+    );
+
+    console.log("Sender balance", senderBalance);
+
+    // Transfer SOL
+    const sig = await program.rpc.transferSol(new anchor.BN(1000000), "Hello", {
+      accounts: {
+        to: wallet.publicKey,
+        from: sender.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        confirmationAccount: pda,
+      },
+      signers: [sender],
+    });
+    console.log("Transfer SOL", sig);
+
+    try {
+      await program.rpc.transferSol(new anchor.BN(1000000), "wrong code", {
+        accounts: {
+          to: wallet.publicKey,
+          from: sender.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          confirmationAccount: pda,
+        },
+        signers: [sender],
+      });
+    } catch (e) {}
+
+    // // Transfer SPL token
+    // await program.rpc.transferSpl(new anchor.BN(10), {
+    //   accounts: {
+    //     fromTokenAccount: fromSplTokenAccount,
+    //     toTokenAccount: toSplTokenAccount,
+    //     fromAuthority: wallet.publicKey,
+    //     tokenProgram: TOKEN_PROGRAM_ID,
+    //   },
+    // });
+  });
+
+  it.skip("Should transfer SPL tokens with the right confirmation code", async () => {
+    const sender = new Keypair();
+    console.log("Sender", sender.publicKey.toString());
+
+    // get pda address
+    const [pda, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [wallet.publicKey.toBuffer(), Buffer.from("ca")],
+      program.programId
+    );
+
+    console.log("PDA", pda.toString());
+
+    // initialize sender account and transfer SOL to it
+    const tx = new Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: sender.publicKey,
+        lamports: 1000000000,
+        space: 0,
+        programId: anchor.web3.SystemProgram.programId,
+      })
+    );
+
+    const sig0 = await connection.sendTransaction(tx, [wallet.payer, sender]);
+    await connection.confirmTransaction(sig0, "confirmed");
+    console.log("Create account", sig0);
+
+    const senderBalance = await connection.getBalance(
+      sender.publicKey,
+      "finalized"
+    );
+    // TODO: send SPL tokens
+    //
+    // await program.rpc.transferSpl(new anchor.BN(10), {
+    //   accounts: {
+    //     fromTokenAccount: fromSplTokenAccount,
+    //     toTokenAccount: toSplTokenAccount,
+    //     fromAuthority: wallet.publicKey,
+    //     tokenProgram: TOKEN_PROGRAM_ID,
+    //   },
+    // });
+  });
+
+  it("Should fails to transfer SOL with wrong confirmation code", async () => {
+    const sender = new Keypair();
+    console.log("Sender", sender.publicKey.toString());
+
+    // get pda address
+    const [pda, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [wallet.publicKey.toBuffer(), Buffer.from("ca")],
+      program.programId
+    );
+
+    console.log("PDA", pda.toString());
+
+    // initialize sender account and transfer SOL to it
+    const tx = new Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: sender.publicKey,
+        lamports: 1000000000,
+        space: 0,
+        programId: anchor.web3.SystemProgram.programId,
+      })
+    );
+
+    const sig0 = await connection.sendTransaction(tx, [wallet.payer, sender]);
+    await connection.confirmTransaction(sig0, "confirmed");
+    console.log("Create account", sig0);
+
+    const senderBalance = await connection.getBalance(
+      sender.publicKey,
+      "finalized"
+    );
+    console.log("Sender balance", senderBalance);
+
+    // Transfer SOL
+    try {
+      const sig = await program.rpc.transferSol(
+        new anchor.BN(1000000),
+        "wrong code",
+        {
+          accounts: {
+            to: wallet.publicKey,
+            from: sender.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+            confirmationAccount: pda,
+          },
+          signers: [sender],
+        }
+      );
+      console.log("Transfer SOL sig", sig);
+      throw new Error("Should fail");
+    } catch (e) {
+      expect(e.error.errorCode.code).eq("InvalidConfirmationCode");
+    }
+
+    const senderBalanceAfter = await connection.getBalance(
+      sender.publicKey,
+      "finalized"
+    );
+    expect(senderBalanceAfter).eq(senderBalance);
   });
 });
